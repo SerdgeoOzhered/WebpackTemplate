0e088b3d6a78971f9c287887146e6a760e9312a3
SergeiOzhered <ser006serg@ukr.net>
2021-04-30 17:32:51 +0300

    new file

--- app/js/app.js
+++ app/js/app.js
@@ -25,4 +25,18 @@ $(document).ready(function() {
 
 	$( ".projects__tabs" ).tabs();
 
-});
+	// Video player
+	const playButton =$("#video__play");
+	const content = $('.video__content');
+
+	playButton.on("click", function() {
+		if (video.paused == true) {
+			video.play();
+			content.addClass('hidden');
+		} else {
+			video.pause();
+			content.removeClass('hidden');
+		}
+	});
+}); 
+
